3c72c1c7391143826b12e57d331a971d1a27d767
Margaret Kiley <mlilliankiley@gmail.com>
2018-01-01 01:09:53 -0500

    Added functionality to form

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/")
-def index():
-    forms = {"Username": "username",
+
+forms = {"Username": "username",
     "Password": "password",
     "Verify Password": "verify",
     "Email (optional)": "email"}
+@app.route("/")
+def index():
     return render_template("form.html", dictionary=forms)
+
+@app.route("/", methods=['POST'])
+def hello():
+    username = request.form['username']
+    password = request.form['password']
+    verify = request.form['verify']
+    email = request.form['email']
+    if username == "" or password == "" or verify == "":
+        return render_template("form.html", dictionary=forms, error="Please enter something!!!")
+    return render_template("welcome.html", person=username)
+
+
 app.run()
